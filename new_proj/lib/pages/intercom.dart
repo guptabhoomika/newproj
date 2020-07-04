80484af1e0e0c56ec942cc3b7a79d35f3194b213
guptabhoomika <43852276+guptabhoomika@users.noreply.github.com>
2020-07-04 19:42:36 +0530

    bottomnavbar

---
+++ new_proj/lib/pages/intercom.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+class Intercom extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Text("aqq")
+      )
+      
+    );
+  }
+}
